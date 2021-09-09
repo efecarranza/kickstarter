cb92bfcba72391d45e59003722a002e936f4a165
piscu Carranza <fcarr004@fiu.edu>
2021-09-09 17:55:00 -0300

    add contract and deployment

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,80 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimumContribution) public {
+        address newCampaign = new Campaign(minimumContribution, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        mapping(address => bool) approvals;
+        uint approvalCount;
+    }
+
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    Request[] public requests;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(manager == msg.sender);
+        _;
+    }
+
+    function Campaign(uint minimum, address managerAddress) public {
+        manager = managerAddress;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(!request.complete);
+        require(request.approvalCount > (approversCount / 2));
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
